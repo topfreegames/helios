7cfc2120553230b604c96bf1b13b39caf7842ad9
hanneli <hannelita@gmail.com>
2013-07-03 20:13:59 -0300

    Fixing json

--- lib/helios/backend/push-notification.rb
+++ lib/helios/backend/push-notification.rb
@@ -93,9 +93,7 @@ class Helios::Backend::PushNotification < Sinatra::Base
     param :payload, String, empty: false
     param :user, String, empty: false
 
-    user = JSON.parse(params[:user])
-
-    tokens = Rack::PushNotification::Device.where(:alias=>user).all.collect(&:token)
+    tokens = Rack::PushNotification::Device.where(:alias=>params[:user]).all.collect(&:token)
 
     options = JSON.parse(params[:payload])
     options[:alert] = options["aps"]["alert"]
