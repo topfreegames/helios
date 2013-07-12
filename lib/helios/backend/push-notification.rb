05f36d7ac587273dea71f1336fcf8262ecb2529f
hanneli <hannelita@gmail.com>
2013-07-12 12:01:11 -0300

    Fixing delete token method

--- lib/helios/backend/push-notification.rb
+++ lib/helios/backend/push-notification.rb
@@ -146,14 +146,8 @@ class Helios::Backend::PushNotification < Sinatra::Base
     # devices = client.devices unless client.nil?
     devices = ["39835C5551C054F9DC15239EAE17DF90984A40611484F6E438A3F604CED7F8F9"]
     unless devices.empty?
-      uri = URI("http://192.168.0.56:5000/push_notification")
-      p uri
       devices.each do |token|
-        request = Net::HTTP::Delete.new("/devices/#{token}")
-        response = Net::HTTP.start(uri.hostname, uri.port) do |http|
-          http.request(request)
-        end
-        p response
+        response = delete "/devices/#{token}"
       end
     end
   end
